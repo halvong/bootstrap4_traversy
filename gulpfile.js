5bea928acb381c64628d997577dd7afe48c4e94f
hal vong <halvong@yahoo.com>
2019-12-30 11:10:14 -0800

    Gulp JS works
    Signed-off-by: hal vong <halvong@yahoo.com>

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,11 @@ var reload      = browserSync.reload;
 // Watch scss AND html files, doing different things with each.
 gulp.task('start', function () {
 
+    //baseDir: "./social"
     // Serve files from the root of this project
     browserSync.init({
         server: {
-            baseDir: "./social"
+            baseDir: "."
         }
     });
 
